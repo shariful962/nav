36107b356818ae79821540b2c307063c0d575075
Shariful Islam <shariful.cse19@gmail.com>
2025-03-15 14:34:12 +0600

    add new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
+
 import Navbar from './component/Navbar'
+import Card from './component/Card';
+import Analyst from './component/Analyst'
+import Newsletter from './component/Newsletter';
 
 function App() {
   
@@ -7,8 +11,11 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Card/>
+      <Analyst/>
+      <Newsletter/>
     </>
   )
 }
 
-export default App
+export default App;
